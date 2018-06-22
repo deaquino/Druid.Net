53591e4e9ac84185f8b99bca8fc349f0214a0df9
Jaime de Aquino <jaime@deaquino.com>
2018-06-22 20:22:27 +0200

    Example formatting

--- DruidDotNet.ConsoleTest/Main.cs
+++ DruidDotNet.ConsoleTest/Main.cs
@@ -12,7 +12,8 @@ namespace DruidDotNet.ConsoleTest
         {
             var request = new IndexSpecBuilder("X")
                 .SetParser("string", "json", "x", "auto")
-                .SetGranularity("uniform", new SimpleGranularity(SimpleGranularityTypes.Year), new SimpleGranularity(SimpleGranularityTypes.None), DateTime.Now, DateTime.Now)
+                .SetGranularity("uniform", new SimpleGranularity(SimpleGranularityTypes.Year),
+                    new SimpleGranularity(SimpleGranularityTypes.None), DateTime.Now, DateTime.Now)
                 .SetFirehose(new LocalFirehose("/x", "*.x"))
                 .SetForceExtendableShard(true)
                 .AddDimensions("dim1", "dim2")
